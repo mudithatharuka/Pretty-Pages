9976544aa14b40992389493912de768d579f3b7f
MudithaTharuka <mudithawyb234432@gmail.com>
2022-12-11 16:09:38 +0530

    nav bar list activator was added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { GoPrimitiveDot } from "react-icons/go";
 import "./styles/NavBar.css";
 
+const getCurrTab = (pathName) => {
+  let page;
+  switch (pathName) {
+    case "/":
+      page = "Home";
+      break;
+    case "/about":
+      page = "About";
+      break;
+    case "/contact":
+      page = "Contact";
+      break;
+    case "/":
+      page = "";
+      break;
+
+    default:
+      page = "";
+      break;
+  }
+  return page;
+};
+
 const NavBar = ({ theme }) => {
+  const location = useLocation();
+  // const [tab, setTab] = useState(getCurrTab(location.pathname));
+
+  const changeActive = (event) => {
+    let siblings = [];
+    let s = event.target.parentNode.parentNode.parentNode.firstChild;
+
+    do {
+      siblings.push(s);
+    } while ((s = s.nextSibling));
+
+    for (let i in siblings) {
+      if (siblings[i].textContent === event.target.textContent) {
+        siblings[i].lastChild.style.visibility = "visible";
+      } else {
+        siblings[i].lastChild.style.visibility = "hidden";
+      }
+    }
+  };
+
   return (
     <div className="nav-bar">
       <ul className="grid-wrapper">
@@ -11,23 +54,41 @@ const NavBar = ({ theme }) => {
           <Link to="/" className={` nav-item-${theme}`}>
             <p>Home</p>
           </Link>
-          <GoPrimitiveDot className="nav-item-icon" />
+          {location.pathname === "/" ? (
+            <GoPrimitiveDot className="nav-item-icon" />
+          ) : (
+            ""
+          )}
         </li>
         <li className={`nav-item `}>
           <Link to="/about" className={`nav-item-${theme}`}>
             <p>About</p>
           </Link>
-          <GoPrimitiveDot className="nav-item-icon" />
+          {location.pathname === "/about" ? (
+            <GoPrimitiveDot className="nav-item-icon" />
+          ) : (
+            ""
+          )}
         </li>
         <li className={`nav-item `}>
           <Link to="/contact" className={`nav-item-${theme}`}>
             <p>Contact</p>
           </Link>
-          <GoPrimitiveDot className="nav-item-icon" />
+          {location.pathname === "/contact" ? (
+            <GoPrimitiveDot className="nav-item-icon" />
+          ) : (
+            ""
+          )}
         </li>
         <li className={`nav-item `}>
-          <p>Home</p>
-          <GoPrimitiveDot className="nav-item-icon" />
+          <Link to="/ex" className={`nav-item-${theme}`}>
+            <p>Extra</p>
+          </Link>
+          {location.pathname === "/ex" ? (
+            <GoPrimitiveDot className="nav-item-icon" />
+          ) : (
+            ""
+          )}
         </li>
       </ul>
     </div>
